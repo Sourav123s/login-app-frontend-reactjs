d40df4c72db999c5cb93bfdf82f931e38bce7c4e
sourav <paramaniksourav10111999@gmail.com>
2022-06-04 15:52:23 +0530

    initial commit

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { useNavigate } from 'react-router-dom'
 import api from './apiCall'
 import { ToastContainer, toast } from 'react-toastify';
